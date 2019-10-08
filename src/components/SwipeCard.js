1a9d71900f6780dd1b8b366a1593af6ac69a5bac
Mark Vonk <markjhvonk@gmail.com>
2019-10-08 22:38:59 +0200

    Added drag trigger for classifier

--- src/components/SwipeCard.js
+++ src/components/SwipeCard.js
@@ -1,26 +1,38 @@
 import React from 'react';
 
 import { motion, useMotionValue, useTransform } from "framer-motion";
+import { identifier } from '@babel/types';
 
 export const SwipeCard = () => {
-  const x = useMotionValue(0);
-  const xInput = [-200, 0, 200];
-  const backgroundColor = useTransform(x, xInput, [
-    "#FFDCDC",
-    "#FFFFFF",
-    "#E6F4DC"
-  ]);
-  const rotate = useTransform(x, xInput, [-45, 0, 45]);
+    function swipeClassifier(e, i){
+        if (i.offset.x >= 250) {
+            console.log(false)
+        } else if (i.offset.x <= -250) {
+            console.log(true);
+        }
+    }
 
-  return (
-      <motion.div
-        drag
-        className="swipe-card"
-        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-        style={{ x, backgroundColor, rotate }}
-      >
-      </motion.div>
-  )
+    const x = useMotionValue(0);
+    const xInput = [-200, 0, 200];
+    const backgroundColor = useTransform(x, xInput, [
+        "#FFDCDC",
+        "#FFFFFF",
+        "#E6F4DC"
+    ]);
+    const rotate = useTransform(x, xInput, [-45, 0, 45]);
+
+    return (
+        <motion.div
+            drag
+            className="swipe-card"
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            style={{ x, backgroundColor, rotate }}
+            onDrag={(e, i) => {
+                swipeClassifier(e,i)
+            }}
+        >
+        </motion.div>
+    )
 }
 
 export default SwipeCard;
