47ce2ec4727e9f1b794a6dfd6df205882c071d48
robintreur <robintreur@gmail.com>
2019-11-22 09:55:18 +0100

    Fixed Special Characters bug

--- src/screens/SwipeTest.js
+++ src/screens/SwipeTest.js
@@ -56,7 +56,39 @@ class SwipeTest extends React.Component {
 
     decode(data) {
         const parser = new DOMParser();
-        const decodedString = parser.parseFromString(`<!doctype html><body>${window.atob(data)}`, 'text/html').body.textContent;
+        let decodedString = parser.parseFromString(`<!doctype html><body>${window.atob(data)}`, 'text/html').body.textContent;
+        decodedString = this.decodeSpecialCharacters(decodedString);
+        return decodedString;
+    }
+
+    decodeSpecialCharacters(decodedString) {
+        decodedString = decodedString.split('Ã«').join('ë');
+        decodedString = decodedString.split('Ã©').join('é');
+        decodedString = decodedString.split('Ã¨').join('è');
+        decodedString = decodedString.split(`Ã `).join('à');
+        decodedString = decodedString.split('Ã¡').join('á');
+        decodedString = decodedString.split('Ã¤').join('ä');
+        decodedString = decodedString.split('Ã¹').join('ù');
+        decodedString = decodedString.split('Ãº').join('ú');
+        decodedString = decodedString.split('Ã¼').join('ü');
+        decodedString = decodedString.split('Ã¶').join('ö');
+        decodedString = decodedString.split('Â©').join('©');
+        decodedString = decodedString.split('Â®').join('®');
+        decodedString = decodedString.split('Ã').join('À');
+        decodedString = decodedString.split('Ã').join('Á');
+        decodedString = decodedString.split('Ã').join('Ä');
+        decodedString = decodedString.split('Ã').join('È');
+        decodedString = decodedString.split('Ã').join('É');
+        decodedString = decodedString.split('Ã').join('Ë');
+        decodedString = decodedString.split('Ã').join('Ö');
+        decodedString = decodedString.split('Ä').join('ć');
+        decodedString = decodedString.split('Ä').join('Ć');
+        decodedString = decodedString.split('Ã').join('Ù');
+        decodedString = decodedString.split('Ã').join('Ú');
+        decodedString = decodedString.split('Ã').join('Ü');
+        decodedString = decodedString.split('â¬Ü').join('‘');
+        decodedString = decodedString.split('â¬"').join('’');
+
         return decodedString;
     }
 
